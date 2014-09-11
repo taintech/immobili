a71c9576cb3e32b2346b23593150b10774ab76ae
Rinat Tainov <rinattainov@gmail.com>
2014-09-11 15:31:54 +0300

    Implemented GoogleSuggest but not tested

--- src/main/scala/com/taintech/immobili/example/GoogleSuggest.scala
+++ src/main/scala/com/taintech/immobili/example/GoogleSuggest.scala
@@ -1,5 +1,9 @@
 package com.taintech.immobili.example
 
+import org.openqa.selenium.By
+import org.openqa.selenium.firefox.FirefoxDriver
+
+import scala.collection.JavaConverters._
 /**
  * Author: Rinat Tainov
  * Email: rinat@tainov.com
@@ -7,5 +11,12 @@ package com.taintech.immobili.example
  * Time: 11:28 AM
  */
 object GoogleSuggest extends App{
-
+  val driver = new FirefoxDriver()
+  driver.get("http://www.google.com/webhp?complete=1&hl=en")
+  val query = driver.findElement(By.name("q"))
+  query.sendKeys("Cheese")
+  Thread.sleep(5000)
+  val allSuggestions = driver.findElements(By.xpath("//td[@class='gssb_a gbqfsf']")).asScala
+  allSuggestions.foreach(e => println(e.getText))
+  driver.quit()
 }
