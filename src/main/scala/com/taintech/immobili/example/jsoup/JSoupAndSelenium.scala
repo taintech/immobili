87765d25c7e9328c8b49c8e65ee8624348715784
taintech <rinattainov@gmail.com>
2014-09-16 01:03:47 +0300

    Finished example with jsoup

--- src/main/scala/com/taintech/immobili/example/jsoup/JSoupAndSelenium.scala
+++ src/main/scala/com/taintech/immobili/example/jsoup/JSoupAndSelenium.scala
@@ -1,5 +1,10 @@
 package com.taintech.immobili.example.jsoup
 
+import org.jsoup.Jsoup
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+
+import scala.collection.JavaConverters._
+
 /**
  * Author: Rinat Tainov
  * Email: rinat@tainov.com
@@ -7,5 +12,11 @@ package com.taintech.immobili.example.jsoup
  * Time: 11:00 PM
  */
 object JSoupAndSelenium {
-  ???
+  def main(args: Array[String]): Unit ={
+    val driver = new HtmlUnitDriver()
+    driver.get("http://krisha.kz/")
+    val doc = Jsoup.parse(driver.getPageSource)
+    doc.select(".titleBlock").asScala.foreach(println)
+    driver.quit()
+  }
 }
